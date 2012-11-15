9901135596fa3f9acc144ec88086695deb601b4e
Andrew Hammond <andrew@evertrue.com>
2012-11-15 14:56:30 -0500

    oops, removed config file

--- include/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$BASE_URL     = "http://github.andrew.evertrue.com";
-$CLIENT_KEY   = "7c459f2d985a942e8437";
-$CLIENT_SEC   = "11ef46f9f515edd5460952037692b887c71ee490";
-$SECRET_NONCE = "2e81c11f644e29cd85df835ba647b2e5";
-
-$GH_AUTH_URL  = "https://github.com/login/oauth/authorize";
-$GH_TOKEN_URL = "https://github.com/login/oauth/access_token";
