d9b0204da6df0b1067cf6b323265a871ddb0a089
Andrew Hammond <andrew@evertrue.com>
2012-11-15 14:55:03 -0500

    simplified config, removing apache configuration from project

---
+++ include/config-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+$BASE_URL     = "<insert a base URL where your application will be available>";
+$CLIENT_KEY   = "<insert your github application client id>";
+$CLIENT_SEC   = "<insert your github application client secret>";
+$SECRET_NONCE = "<insert a secret phrase here>";
+
+$GH_AUTH_URL  = "https://github.com/login/oauth/authorize";
+$GH_TOKEN_URL = "https://github.com/login/oauth/access_token";
--- include/config.php
+++ include/config.php
@@ -1,8 +1,9 @@
 <?php
 
-$BASE_URL     = getenv('BASE_URL');
-$CLIENT_KEY   = getenv('GH_KEY');
-$CLIENT_SEC   = getenv('GH_SEC');
-$SECRET_NONCE = getenv('SHHHH_ITS_SECRET');
-$GH_AUTH_URL  = getenv('GH_AUTH_URL');
-$GH_TOKEN_URL = getenv('GH_TOKEN_URL');
+$BASE_URL     = "http://github.andrew.evertrue.com";
+$CLIENT_KEY   = "7c459f2d985a942e8437";
+$CLIENT_SEC   = "11ef46f9f515edd5460952037692b887c71ee490";
+$SECRET_NONCE = "2e81c11f644e29cd85df835ba647b2e5";
+
+$GH_AUTH_URL  = "https://github.com/login/oauth/authorize";
+$GH_TOKEN_URL = "https://github.com/login/oauth/access_token";
